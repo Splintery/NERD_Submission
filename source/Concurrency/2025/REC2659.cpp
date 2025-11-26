02f1159f7d5b48bc4d65064ed7ae9b5d4b3fa2e2
Candidate REC2659 <REC2659@nohost.null>
2025-11-26 18:57:11 +0100

    Pseudo code for COncurrency exercise
    Wrote the pseudo code plan for aquiring the initial message in the
    message handler. First step for initialisation of the structure used to
    redirect messages.

--- source/Concurrency/2025/REC2659.cpp
+++ source/Concurrency/2025/REC2659.cpp
@@ -1,5 +1,7 @@
 #include "REC2659.h"
 #include <Helpers.h>
+#include <ostream>
+#include <iostream>
 
 namespace REC2659
 {
@@ -7,6 +9,12 @@ namespace REC2659
 void Initialize(size_t thread_count)
 {
     UNUSED_PARAM(thread_count);
+    std::cout << "Initialising thread mail boxes" << std::endl;
+    // while (thread_count --> 0)
+    // {
+    //     thread_mailboxes.at(thread_count).push(Message());
+    //     std::cout << "Pushed messqge on queue of thread[" << thread_count << "]" << std::endl;
+    // }
 }
 
 void Cleanup()
@@ -21,7 +29,32 @@ void Cleanup()
  * @param message_handler the message handler of the current thread
  */
 void HandleMessages(size_t thread_index, MessageHandler& message_handler)
-{
+{   
+    /* pseudo-code phase
+    while (message_handler.HasMessageToSend()) // Work until there is no work !
+    {
+        
+        if (mutex.try_lock())
+        {
+            std::optional<Message> message = message_handler.GetMessageToSend();
+            if (it is my message)
+            {
+                message_handler.ReceivedMessage(message.value())
+            }
+            else
+            {
+                pass it to the identified thread queue
+            }
+            mutex.unlock()
+            // Check if the message we got concerns our thread
+        }
+        else // This means another thread got the message
+        {
+            // Wait until another thread passes the message onto us
+        }
+    }
+    */
+
     UNUSED_PARAM(thread_index);
     UNUSED_PARAM(message_handler);
 }
--- source/Concurrency/2025/REC2659.h
+++ source/Concurrency/2025/REC2659.h
@@ -1,10 +1,12 @@
 #ifndef REC2659_H
 #define REC2659_H
 #include "context.h"
+#include <vector>
+#include <queue>
 
 namespace REC2659
 {
-	
+static std::vector<std::queue<Message>> thread_mailboxes;
 void Initialize(size_t thread_count);
 void Cleanup();
 
